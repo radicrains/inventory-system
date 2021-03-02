5f6b8906e620b18ef8c70ae158e19417c32a108e
Juliana <juliana.mohamedali@gmail.com>
2021-03-02 12:45:13 +0800

    added user CRUD

--- controllers/apps.js
+++ controllers/apps.js
@@ -2,53 +2,93 @@ const express = require('express');
 const apps = express.Router();
 
 const Tools = require('../models/tools.js');
+const User = require('../models/users.js');
 const seedController = require('./seed.js');
 
 
-apps.get('/', (req, res)=>{
+const isAuthenticated = (req, res, next) => {
+    if (req.session.currentUser) {
+        return next()
+    } else {
+        res.redirect('/sessions/new');
+    }
+};
+
+// apps.get('/', isAuthenticated, (req, res)=>{
+//     if(req.session.currentUser){
+//         res.render('apps/dashboard.ejs');
+//     } else {
+//         res.redirect('/sessions/new');
+//     }
+// });
+
+apps.get('/', isAuthenticated, (req, res)=>{
     if(req.session.currentUser){
-        res.render('apps/dashboard.ejs');
+        User.findById(req.session.currentUser._id, (err, foundUser) => {
+            res.render('apps/dashboard.ejs', {
+                user: foundUser,
+            });
+        });
     } else {
         res.redirect('/sessions/new');
     }
 });
+
 
 //User Routes via Controller
 apps.use('/seed', seedController);
 
 //CREATE A FORM FOR ADDING IN NEW TOOL
 apps.get('/add', (req, res) => {
-    res.render('apps/new.ejs');
+    User.findById(req.session.currentUser._id, (err, foundUser) => {
+        res.render('apps/new.ejs', {
+            user: foundUser,
+        });
+    });
+    // res.render('apps/new.ejs');
 });
 
 apps.post('/list', (req,res) => {
     Tools.create(req.body, (err, createTool) => {
         res.redirect('/apps');
     });
+    
 });
 
 
 // CREATE INDEX ROUTE
-apps.get('/list', (req, res) => {
-    Tools.find({}, (err, allTools) => {
-        res.render('apps/index.ejs', {
-            tools: allTools
+apps.get('/list', isAuthenticated, (req, res) => {
+    User.findById(req.session.currentUser._id, (err, foundUser) => {
+        Tools.find({}, (err, allTools) => {
+            res.render('apps/index.ejs', {
+                tools: allTools,
+                user: foundUser,
+            });
         });
     });
+    // User.findById(req.session.currentUser._id, (err, foundUser) => {
+    //     res.render('apps/index.ejs', {
+    //         user: foundUser,
+    //     });
+    // });
 });
 
 
 // CREATE SHOW ROUTE
-apps.get('/:id', (req, res) => {
-    Tools.findById(req.params.id, (err, foundTool) => {
-        res.render('apps/show.ejs', {
-            tools: foundTool,
+apps.get('/:id', isAuthenticated, (req, res) => {
+    User.findById(req.session.currentUser._id, (err, foundUser) => {
+        Tools.findById(req.params.id, (err, foundTool) => {
+            res.render('apps/show.ejs', {
+                tools: foundTool,
+                user: foundUser,
+            });
         });
     });
+    
 });
 
 //CREATE DELETE ROUTE
-apps.delete('/:id',(req,res) => {
+apps.delete('/:id',isAuthenticated, (req,res) => {
     Tools.findByIdAndRemove(req.params.id, (err, data) => {
         res.redirect('/apps/list');
     });
@@ -61,15 +101,19 @@ apps.delete('/:id',(req,res) => {
 // });
 
 //CREATE EDIT ROUTE
-apps.get('/:id/edit', (req, res) => {
-    Tools.findById(req.params.id, (err,foundTool) => {
-        res.render('apps/edit.ejs', {
-            tools: foundTool,
+apps.get('/:id/edit', isAuthenticated, (req, res) => {
+    User.findById(req.session.currentUser._id, (err, foundUser) => {
+        Tools.findById(req.params.id, (err,foundTool) => {
+            res.render('apps/edit.ejs', {
+                tools: foundTool,
+                user: foundUser,
+            });
         });
     });
+    
 });
 
-apps.put('/:id', (req, res) => {
+apps.put('/:id', isAuthenticated, (req, res) => {
     Tools.findByIdAndUpdate(req.params.id, req.body, {new: true}, (err, updateTool) => {
         if(err) {
             console.log(err.message);
--- controllers/sessions.js
+++ controllers/sessions.js
@@ -3,14 +3,9 @@ const sessions = express.Router();
 const User = require('../models/users.js');
 const bcrypt = require('bcrypt');
 
+
 sessions.get('/new', (req, res) => {
     res.render('sessions/new.ejs');
-});
-
-sessions.get('/', (req, res) => {
-    res.render('index.ejs', {
-        currentUser: req.session.currentUser
-    });
 });
 
 sessions.post('/', (req, res) => {
@@ -34,6 +29,61 @@ sessions.post('/', (req, res) => {
     });
 });
 
+sessions.get('/', (req, res) => {
+    res.render('index.ejs', {
+        currentUser: req.session.currentUser
+    });
+});
+
+//User show ejs
+sessions.get('/:id', (req,res) => {
+    // console.log(req.session.currentUser)
+    User.findById(req.session.currentUser._id, (err, foundUser) => {
+        res.render('sessions/show.ejs', {
+            user: foundUser,
+        });
+    });
+});
+
+//User INFO edit ejs
+sessions.get('/:id/edit', (req,res) => {
+    // console.log(req.session.currentUser)
+    User.findById(req.session.currentUser._id, (err, foundUser) => {
+        res.render('sessions/edit.ejs', {
+            user: foundUser,
+        });
+    });
+});
+
+sessions.put('/:id', (req, res) => {
+    User.findByIdAndUpdate(req.session.currentUser._id, req.body, {new:true}, (err, updateUser) => {
+        if(err) {
+            console.log(err.message);
+        } else {
+            res.redirect('/sessions/'+req.session.currentUser._id);
+        }
+    })
+})
+
+//User Delete Account and end session (FOR FUTURE: TO CHANGE WHEN HAVE MULTIPLE USERS - ADMIN TO SEE ALL USERS & DELETE)
+sessions.delete('/:id', (req, res) => {
+    User.findByIdAndRemove(req.session.currentUser._id, (err, data) => {
+        req.session.destroy(() => {
+            res.redirect('/');
+        });
+    });
+});
+
+
+// apps.delete('/:id',isAuthenticated, (req,res) => {
+//     Tools.findByIdAndRemove(req.params.id, (err, data) => {
+//         res.redirect('/apps/list');
+//     });
+// });
+
+
+
+//User logout/delete session
 sessions.delete('/', (req, res) => {
     req.session.destroy(() => {
         res.redirect('/');
